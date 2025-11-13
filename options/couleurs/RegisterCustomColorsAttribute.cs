246458c0127ec3948e3b15accf1cffd7f93d6c91
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-11-13 13:57:36 +0100

    Add files via upload
    Signed-off-by: Antho_Off <113532396+AnthoYt@users.noreply.github.com>

---
+++ options/couleurs/RegisterCustomColorsAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MiraAPI.Colors;
+
+/// <summary>
+/// Used to mark a class for custom colors registration.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class RegisterCustomColorsAttribute : Attribute;
