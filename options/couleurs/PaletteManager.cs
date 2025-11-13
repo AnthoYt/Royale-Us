f8067c5fd6b8f825213578010df816a7b612dea9
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-11-13 13:57:20 +0100

    Add files via upload
    Signed-off-by: Antho_Off <113532396+AnthoYt@users.noreply.github.com>

---
+++ options/couleurs/PaletteManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+
+namespace MiraAPI.Colors;
+
+/// <summary>
+/// Used to register and track custom colors.
+/// </summary>
+public static class PaletteManager
+{
+    /// <summary>
+    /// Gets all registered custom colors.
+    /// </summary>
+    public static CustomColor[] RegisteredColors => [.. CustomColors];
+
+    internal static readonly List<CustomColor> CustomColors = [];
+
+    internal static void RegisterAllColors()
+    {
+        var colors = CustomColors.Select(x => x.MainColor).ToArray();
+        var shadowColors = CustomColors.Select(x => x.ShadowColor).ToArray();
+        var stringNames = CustomColors.Select(x => x.Name).ToArray();
+
+        Palette.PlayerColors = Palette.PlayerColors.ToArray().AddRangeToArray(colors);
+        Palette.ShadowColors = Palette.ShadowColors.ToArray().AddRangeToArray(shadowColors);
+        Palette.ColorNames = Palette.ColorNames.ToArray().AddRangeToArray(stringNames);
+
+        Palette.TextColors = Palette.TextColors.ToArray().AddRangeToArray(colors);
+        Palette.TextOutlineColors = Palette.TextOutlineColors.ToArray().AddRangeToArray(shadowColors);
+    }
+}
